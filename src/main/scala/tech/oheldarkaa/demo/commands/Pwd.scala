a5930dea68666447c23b89c3ccc230c621b2e677
Eldar Dragomir <oh.eldarkaa@gmail.com>
2021-08-12 15:05:49 +0300

    Commands: Pwd, Touch (stubs)

---
+++ src/main/scala/tech/oheldarkaa/demo/commands/Pwd.scala
@@ -0,0 +1,7 @@
+package tech.oheldarkaa.demo.commands
+
+import tech.oheldarkaa.demo.filesystem.State
+
+class Pwd extends Command {
+  def apply(state: State): State = ???
+}
---
+++ src/main/scala/tech/oheldarkaa/demo/commands/Touch.scala
@@ -0,0 +1,7 @@
+package tech.oheldarkaa.demo.commands
+
+import tech.oheldarkaa.demo.filesystem.State
+
+class Touch extends Command {
+  def apply(state: State): State = ???
+}
